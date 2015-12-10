4fca5871a9ea1d42d10e909ead182cc84ffbabba
ZhihaoXie <xiezhhao90@163.com>
2015-12-10 16:09:13 +0800

    change compute object from CDS to gene

--- gbk-summary.pl
+++ gbk-summary.pl
@@ -18,7 +18,7 @@ my $fgbk = basename $gbk_file;
 
 print "Id\tgene number\tgene length(bp)\tgene avg length(bp)\tgene/genome(%)\tGC in genes\tgene density(per Kb)\trRNA number\ttRNA number\n";
 
-my ($all_gene,$all_length,$all_ave,$all_rrna,$all_trna,$all_gc,$gc);
+my ($all_gene,$all_length,$all_avg,$all_rrna,$all_trna,$all_gc,$gc);
 $all_rrna=0;
 $all_trna=0;
 my $sall_length;
@@ -26,18 +26,18 @@ my $all_density = 0;
 
 my $seq_obj=Bio::SeqIO->new(-file=>"< $gbk_file",-format=>'genbank');
 while (my $seq=$seq_obj->next_seq()) {
-    my $num_cds=0;my $length_cds=0;my $num_rrna=0;my $num_trna=0;my $agv_cds=0;my $gc_num=0;my $gc_cds=0;
+    my $num_cds=0;my $length_cds=0;my $num_rrna=0;my $num_trna=0;my $avg_cds=0;my $gc_num=0;my $gc_cds=0;
     my $density = 0;
     my $id = $seq->id;
     my $seq_length = $seq->length;
     $sall_length += $seq_length;
     my @features = $seq->all_SeqFeatures;
     for my $f (@features) {
-        if ($f->primary_tag eq 'CDS') {
+        if ($f->primary_tag eq 'CDS' || $f->primary_tag eq 'rRNA' || $f->primary_tag eq 'tRNA') {
             $num_cds += 1;
+            $all_gene += 1;
             my $l_cds = $f->length;
             $length_cds += $l_cds;
-            $all_gene += 1;
             $all_length += $l_cds;
             my $start = $f->start;
             my $end = $f->end;
@@ -45,18 +45,18 @@ while (my $seq=$seq_obj->next_seq()) {
             my $number = $str =~ tr/GCgc/GCgc/;
             $gc_num += $number;
             $all_gc += $number;
-        }
-        if ($f->primary_tag eq 'rRNA') {
-            $num_rrna += 1;
-            $all_rrna += 1;
-        }
-        if ($f->primary_tag eq 'tRNA') {
-            $num_trna += 1;
-            $all_trna += 1;
+            if ($f->primary_tag eq 'rRNA') {
+                $num_rrna += 1;
+                $all_rrna += 1;
+            }
+            if ($f->primary_tag eq 'tRNA') {
+                $num_trna += 1;
+                $all_trna += 1;
+            }
         }
     }
     if ($num_cds > 0) {
-        $agv_cds = $length_cds/$num_cds;
+        $avg_cds = $length_cds/$num_cds;
     }
     if ($length_cds > 0) {
         $gc_cds = $gc_num/$length_cds*100; 
@@ -65,16 +65,17 @@ while (my $seq=$seq_obj->next_seq()) {
     $density = ($num_cds/$seq_length) * 1000;
 
     print "$id\t$num_cds\t$length_cds\t";
-    printf "%0.2f\t%0.2f%%\t%0.2f%%\t%0.2f", $agv_cds,$ratio,$gc_cds,$density;
+    printf "%0.2f\t%0.2f%%\t%0.2f%%\t%0.2f", $avg_cds,$ratio,$gc_cds,$density;
     print "\t$num_rrna\t$num_trna\n";
 }
 if ($all_gene > 0) {
-    $all_ave = $all_length/$all_gene;
+    $all_avg = $all_length/$all_gene;
     $gc = $all_gc/$all_length*100;
 }
 my $all_ratio = $all_length/$sall_length*100;
 $all_density = $all_gene/$sall_length*1000;
 print "#SUM:\n";
 print "$fgbk\t$all_gene\t$all_length\t";
-printf "%0.2f\t%0.2f%%\t%0.2f%%\t%0.2f", $all_ave,$all_ratio,$gc,$all_density;
+printf "%0.2f\t%0.2f%%\t%0.2f%%\t%0.2f", $all_avg,$all_ratio,$gc,$all_density;
 print "\t$all_rrna\t$all_trna\n";
+
